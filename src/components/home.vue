aa79e04c1dead1578bd0d4223bcc2daba97adf0d
WYseven <consolelog@126.com>
2017-05-09 20:34:25 +0800

    增加首页布局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="main">
+    <div class="home-header">
+      <img src="../assets/miaov.png" alt="">
+      <div class="portrait">
+        <span>登录/注册</span>
+        <img src="../assets/portrait.png" alt="">
+      </div>
+    </div>
     <div class="btns">
       <router-link to="/project" tag="div" class=" trans">我的项目</router-link>
       <router-link to="/doc" tag="div" class=" trans">我的文档</router-link>
--- src/views/backend/code.vue
+++ src/views/backend/code.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="block-content">
       这是工作台页面
-  </div>
+    </div>
   </div>
 </template>
 
