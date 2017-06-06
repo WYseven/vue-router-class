ced1c82d6fa97a329424562e07a613271e324baa
WYseven <consolelog@126.com>
2017-06-06 11:20:48 +0800

    使用element组件

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import VueAxios from 'vue-axios'
 import store from './store'
 import { sync } from 'vuex-router-sync'
 
+import {MessageBox, Message} from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+
 sync(store, router)
 
 Vue.use(VueAxios, axios)
--- src/views/backend/project.vue
+++ src/views/backend/project.vue
@@ -4,32 +4,64 @@
       <i class='fa fa-plus-square'></i>
       <span>创建项目</span>
     </div>
-    <div class="content-box">
-      <div class="block-content">
-          这是我的项目页面
+    <div class="content-main"  id='page-group'>
+      <div v-for="item,index in projectList" class="grid-content trans">
+          <i class="fa fa-sun-o trans"></i>
+          <p class="group-name">{{item.title}}</p>
+          <i class="fa fa-trash-o remove" @click="removeHandle(index)"></i>
       </div>
     </div>
-    
-      <modal :show.sync='modalShow' />
   </div>
 </template>
 
 <script>
-  import modal from '@/components/commcoms/modal'
 
   export default {
     name: 'Project',
-    components: {
-      modal
-    },
     data () {
       return {
-        modalShow: false
+        show: false,
+        projectList: []
       }
     },
     methods: {
       addProject () {
-        this.modalShow = true
+        // this.show = true
+        this.$prompt(' ', '写入项目名称', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /.+/,
+          inputErrorMessage: '请输入项目名称'
+        })
+        .then(({value}) => {
+          this.projectList.push({
+            title: value
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消输入'
+          })
+        })
+      },
+      removeHandle (index) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          this.projectList.splice(index, 1)
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
       }
     },
     beforeRouteEnter (to, from, next) {
